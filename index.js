cd08b7e0af570de25b16e22c606f7ca64d32a146
Savannah C <sgcorrell@gmail.com>
2019-10-01 17:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
     .then(function(response) {
       return response.json();
-    });    
+    });
 }
 
 function renderBooks(json) {
